2395f7ffa3561c23f41dcc7fc2fcbda253ed32b7
clerio006 <66549079+clerio006@users.noreply.github.com>
2020-06-07 12:20:04 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
-const io = require('socket.io')(3000)
+const express = require('express')
+const app = express()
 
-const users = {}
+app.use(express.static("public"))
 
-io.on('connection', socket => {
-  socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
-  })
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-  })
+const http = require('http').Server(app)
+const serverSocket = require('socket.io')(http)
+
+const porta = process.env.PORT || 8000
+
+const host = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : "http://localhost"
+
+http.listen(porta, function(){
+    const portaStr = porta === 80 ? '' :  ':' + porta
+
+    if (process.env.HEROKU_APP_NAME) 
+        console.log('Servidor iniciado. Abra o navegador em ' + host)
+    else console.log('Servidor iniciado. Abra o navegador em ' + host + portaStr)
 })
+
+app.get('/', function (requisicao, resposta) {
+    resposta.sendFile(__dirname + '/index.html')
+})
+
+
+serverSocket.on('connect', function(socket){
+    socket.on('login', function (nickname) {
+        socket.nickname = nickname
+        const msg = nickname + ' conectou'
+        console.log(msg)
+        serverSocket.emit('chat msg', msg)
+    })
+
+    socket.on('disconnect', function(){
+        console.log('Cliente desconectado: ' + socket.nickname)
+    })
+        
+    socket.on('chat msg', function(msg){
+        serverSocket.emit('chat msg', `${socket.nickname} diz: ${msg}`)
+    })
+
+    socket.on('status', function(msg){
+        console.log(msg)
+        socket.broadcast.emit('status', msg)
+    })
+})
+
